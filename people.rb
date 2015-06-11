94d9d839e92f160d5e8b47c17fef7da997b80097
Nancy Wood <nancywood13@gmail.com>
2015-06-11 14:55:10 -0400

    parent class with greeting method

--- people.rb
+++ people.rb
@@ -1,5 +1,21 @@
+class Person
+	def initialize(name)
+		@name = name 
+	end
+	def greeting
+		puts "Hi, my name is #{name}"
+	end
+end
+
 class Student
+	def learn
+		puts "I get it!"
+	end
 end
 
 class Instructor
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
 end
+
